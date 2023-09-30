61a39e4332cd8b3eb8729189c97b345b7db42258
Quentin Balestri <quentinbalestri@gmail.com>
2023-09-30 12:54:10 -0500

    thermal stuff

--- VMC/sandbox/sandbox.py
+++ VMC/sandbox/sandbox.py
@@ -1,79 +1,104 @@
-# Here we import our own MQTT library which takes care of a lot of boilerplate
-# code related to connecting to the MQTT server and sending/receiving messages.
-# It also helps us make sure that our code is sending the proper payload on a topic
-# and is receiving the proper payload as well.
+import json, base64, cv2
+import numpy as np
+from threading import Thread
+from scipy import ndimage
 from bell.avr.mqtt.client import MQTTModule
-from bell.avr.mqtt.payloads import AvrFcmVelocityPayload
-
-# This imports the third-party Loguru library which helps make logging way easier
-# and more useful.
-# https://loguru.readthedocs.io/en/stable/
+from bell.avr.mqtt.payloads import *
 from loguru import logger
 
 
-# This creates a new class that will contain multiple functions
-# which are known as "methods". This inherits from the MQTTModule class
-# that we imported from our custom MQTT library.
 class Sandbox(MQTTModule):
-    # The "__init__" method of any class is special in Python. It's what runs when
-    # you create a class like `sandbox = Sandbox()`. In here, we usually put
-    # first-time initialization and setup code. The "self" argument is a magic
-    # argument that must be the first argument in any class method. This allows the code
-    # inside the method to access class information.
     def __init__(self) -> None:
-        # This calls the original `__init__()` method of the MQTTModule class.
-        # This runs some setup code that we still want to occur, even though
-        # we're replacing the `__init__()` method.
         super().__init__()
-        # Here, we're creating a dictionary of MQTT topic names to method handles.
-        # A dictionary is a data structure that allows use to
-        # obtain values based on keys. Think of a dictionary of state names as keys
-        # and their capitals as values. By using the state name as a key, you can easily
-        # find the associated capital. However, this does not work in reverse. So here,
-        # we're creating a dictionary of MQTT topics, and the methods we want to run
-        # whenever a message arrives on that topic.
-        self.topic_map = {"avr/fcm/velocity": self.show_velocity}
+        self.topic_map = {
+            'avr/thermal/reading': self.handle_thermal,
+            'avr/fcm/status': self.handle_status,
+            'avr/autonomous/building/drop': self.handle_drop,
+            'avr/autonomous/enable': self.handle_autonomous,
+            }
+        
+        self.autonomous: bool = False
+        self.auto_target: bool = False
+        self.status_loop: bool = False
+        
+        self.is_armed: bool = False
+        self.building_drops: dict  = {'Building 1': False, 'Building 2': False, 'Building 3': False, 'Building 4': False, 'Building 5': False}
+        self.thermal_pixel_matrix = [[0]*8]*8
 
-    # Here's an example of a custom message handler here.
-    # This is what executes whenever a message is received on the "avr/fcm/velocity"
-    # topic. The content of the message is passed to the `payload` argument.
-    # The `AvrFcmVelocityMessage` class here is beyond the scope of AVR.
-    def show_velocity(self, payload: AvrFcmVelocityPayload) -> None:
-        vx = payload["vX"]
-        vy = payload["vY"]
-        vz = payload["vZ"]
-        v_ms = (vx, vy, vz)
-
-        # Use methods like `debug`, `info`, `success`, `warning`, `error`, and
-        # `critical` to log data that you can see while your code runs.
-
-        # This is what is known as a "f-string". This allows you to easily inject
-        # variables into a string without needing to combine lots of strings together.
-        # https://realpython.com/python-f-strings/#f-strings-a-new-and-improved-way-to-format-strings-in-python
-        logger.debug(f"Velocity information: {v_ms} m/s")
-
-    # Here is an example on how to publish a message to an MQTT topic to
-    # perform an action
-    def open_servo(self) -> None:
-        # It's super easy, use the `self.send_message` method with the first argument
-        # as the topic, and the second argument as the payload.
-        # Pro-tip, if you set `python.analysis.typeCheckingMode` to `basic` in you
-        # VS Code preferences, you'll get a red underline if your payload doesn't
-        # match the expected format for the topic.
-        self.send_message(  # type: ignore (to appease type checker)
-            "avr/pcm/set_servo_open_close",
-            {"servo": 0, "action": "open"},
+    def handle_thermal(self, payload: AvrThermalReadingPayload) -> None:
+        data = json.loads(payload)['data']
+        base64_decoded = data.encode('utf-8')
+        as_bytes = base64.b64decode(base64_decoded)
+        thermal_pixel_ints = list(bytearray(as_bytes))
+        i = 0
+        for row in range(len(self.thermal_pixel_matrix[0])):
+            for col in range(len(self.thermal_pixel_matrix)):
+                self.thermal_pixel_matrix[row][col] = thermal_pixel_ints[i]
+                i += 1
+        
+    def handle_status(self, payload: AvrFcmStatusPayload) -> None:
+        armed = payload['armed']
+        self.is_armed = armed
+        
+    def handle_drop(self, payload: AvrAutonomousBuildingDropPayload) -> None:
+        self.building_drops[self.building_drops.keys()[payload['id']]] = payload['enabled']
+    
+    def handle_autonomous(self, payload: AvrAutonomousEnablePayload) -> None:
+        self.autonomous = payload['enabled']
+        
+    def fire_cannon(self) -> None:
+        self.send_message(
+            'avr/pcm/fire_laser',
+            AvrPcmFireLaserPayload()
         )
+        
+        
+    def targeting(self) -> None:
+        while self.auto_target:
+            thermal_image = np.asarray(self.thermal_pixel_matrix, dtype=np.uint8)
+            
+            lowerb = np.array([0, 0, 150], np.uint8)
+            upperb = np.array([90, 90, 255], np.uint8)
+
+            frame = cv2.inRange(thermal_image, lowerb, upperb)
+            blobs = frame > 100
+            labels, nlabels = ndimage.label(blobs)
+            # find the center of mass of each label
+            t = ndimage.center_of_mass(frame, labels, np.arange(nlabels) + 1 )
+            # calc sum of each label, this gives the number of pixels belonging to the blob
+            s  = ndimage.sum(blobs, labels,  np.arange(nlabels) + 1 )
+            # print the center of mass of the largest blob
+            heat_center = [int(x) for x in t[s.argmax()][::-1]]
+            print(heat_center)
+            
+            if heat_center[0] > frame.shape[0]/2:
+                pass
+            
+    
+    def status(self) -> None:
+        while self.status_loop:
+            pass
+    
+    def Autonomous(self):
+        while self.autonomous:
+            pass
+        
 
 
-if __name__ == "__main__":
-    # This is what actually initializes the Sandbox class, and executes it.
-    # This is nested under the above condition, as otherwise, if this file
-    # were imported by another file, these lines would execute, as the interpreter
-    # reads and executes the file top-down. However, whenever a file is called directly
-    # with `python file.py`, the magic `__name__` variable is set to "__main__".
-    # Thus, this code will only execute if the file is called directly.
+if __name__ == '__main__':
     box = Sandbox()
-    # The `run` method is defined by the inherited `MQTTModule` class and is a
-    # convience function to start processing incoming MQTT messages infinitely.
+    
+    #Create Threads
+    targeting_thread = Thread(target=box.targeting)
+    targeting_thread.setDaemon(True)
+    targeting_thread.start()
+    
+    status_thread = Thread(target=box.status)
+    status_thread.setDaemon(True)
+    status_thread.start()
+    
+    autonomous_thread = Thread(target=box.Autonomous)
+    autonomous_thread.setDaemon(True)
+    autonomous_thread.start()
+    
     box.run()
