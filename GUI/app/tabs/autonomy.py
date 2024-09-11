269e605a0fe90c0ecd32972206b15b854c289b70
Max Haberer <mhaberer25@gmail.com>
2024-09-11 12:57:50 -0500

    Refactor, organize, and format autonomy widget
    - Slightly altered the positions of some widgets, mostly changing the way that they're organized to make the GUI easier to understand
    - Organized some code blocks, moving certain commands around to make things easier to understand
    - Formatted code to the `black` code style

--- GUI/app/tabs/autonomy.py
+++ GUI/app/tabs/autonomy.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-import functools, json, os, playsound # Use functools.partial to assign different button press actions to buttons inside for-loops
+import functools, json, playsound  # Use functools.partial to assign different button press actions to buttons inside for-loops
 from typing import List, Dict
 
 from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
@@ -29,7 +29,6 @@ class AutonomyWidget(BaseTabWidget):
         """
         Build the GUI layout
         """
-        # print(os.getcwd())
         layout = QtWidgets.QGridLayout(self)
         self.setLayout(layout)
 
@@ -37,9 +36,12 @@ class AutonomyWidget(BaseTabWidget):
         sandbox_groupbox = QtWidgets.QGroupBox("Sandbox")
         sandbox_layout = QtWidgets.QVBoxLayout()
         sandbox_groupbox.setLayout(sandbox_layout)
+        sandbox_groupbox.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Fixed)
+        layout.addWidget(sandbox_groupbox, 0, 0, 1, 3)
 
         # Autonomous control layout
         autonomous_layout = QtWidgets.QHBoxLayout()
+        sandbox_layout.addLayout(autonomous_layout)
 
         autonomous_enable_button = QtWidgets.QPushButton("Enable Auton")
         autonomous_enable_button.clicked.connect(lambda: self.set_autonomous(True))  # type: ignore
@@ -54,13 +56,11 @@ class AutonomyWidget(BaseTabWidget):
         self.autonomous_label.setSizePolicy(QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed))
         autonomous_layout.addWidget(self.autonomous_label)
 
-        sandbox_layout.addLayout(autonomous_layout)
-
-        # Sandbox threads status
+        # Sandbox thread status
         module_status_layout = QtWidgets.QHBoxLayout()
+        sandbox_layout.addLayout(module_status_layout)
 
-        # data structure to hold the topic prefixes and the corresponding widget
-        self.topic_status_map: Dict[str, StatusLabel] = {}
+        self.topic_status_map: Dict[str, StatusLabel] = {}  # data structure to hold the topic prefixes and the corresponding widget
 
         auto_status = StatusLabel("Autonomous Thread")
         self.topic_status_map["Autonomous"] = auto_status
@@ -73,38 +73,34 @@ class AutonomyWidget(BaseTabWidget):
         thermal_status = StatusLabel("Thermal Thread")
         self.topic_status_map["Thermal"] = thermal_status
         module_status_layout.addWidget(thermal_status)
+        # endregion
 
-        sandbox_layout.addLayout(module_status_layout)
-
-        sandbox_groupbox.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Fixed)
-        layout.addWidget(sandbox_groupbox, 0, 0, 1, 1)
-
-        custom_layout = QtWidgets.QHBoxLayout()
-        
         # region Thermal & Laser
-        thermal_laser_groupbox = QtWidgets.QGroupBox('Thermal and Laser Operations')
+        thermal_laser_groupbox = QtWidgets.QGroupBox("Thermal and Laser Operations")
         thermal_laser_layout = QtWidgets.QVBoxLayout()
         thermal_laser_groupbox.setLayout(thermal_laser_layout)
+        thermal_laser_groupbox.setMaximumWidth(350)
+        layout.addWidget(thermal_laser_groupbox, 1, 0, 1, 1)
 
         # thermal control buttons
         thermal_buttons_layout = QtWidgets.QHBoxLayout()
+        thermal_laser_layout.addLayout(thermal_buttons_layout)
 
-        thermal_tracking_button = QtWidgets.QPushButton('Start Tracking')
+        thermal_tracking_button = QtWidgets.QPushButton("Start Tracking")
         thermal_tracking_button.clicked.connect(lambda: self.set_thermal_data(2))
         thermal_buttons_layout.addWidget(thermal_tracking_button)
 
-        thermal_scanning_button = QtWidgets.QPushButton('Start Scanning')
+        thermal_scanning_button = QtWidgets.QPushButton("Start Scanning")
         thermal_scanning_button.clicked.connect(lambda: self.set_thermal_data(1))
         thermal_buttons_layout.addWidget(thermal_scanning_button)
-        
-        thermal_stop_button = QtWidgets.QPushButton('Stop All')
+
+        thermal_stop_button = QtWidgets.QPushButton("Stop All")
         thermal_stop_button.clicked.connect(lambda: self.set_thermal_data(0))
         thermal_buttons_layout.addWidget(thermal_stop_button)
 
-        thermal_laser_layout.addLayout(thermal_buttons_layout)
-        
         # temp range/step settings
         temp_range_layout = QtWidgets.QFormLayout()
+        thermal_laser_layout.addLayout(temp_range_layout)
 
         self.temp_min_line_edit = FloatLineEdit()
         temp_range_layout.addRow(QtWidgets.QLabel("Min:"), self.temp_min_line_edit)
@@ -113,7 +109,7 @@ class AutonomyWidget(BaseTabWidget):
         self.temp_max_line_edit = FloatLineEdit()
         temp_range_layout.addRow(QtWidgets.QLabel("Max:"), self.temp_max_line_edit)
         self.temp_max_line_edit.setText(str(config.temp_range[1]))
-        
+
         self.temp_step_edit = FloatLineEdit()
         temp_range_layout.addRow(QtWidgets.QLabel("Step:"), self.temp_step_edit)
         self.temp_step_edit.setText(str(config.temp_range[2]))
@@ -121,9 +117,7 @@ class AutonomyWidget(BaseTabWidget):
         set_temp_range_button = QtWidgets.QPushButton("Update Thermal Params")
         set_temp_range_button.clicked.connect(lambda: self.set_thermal_data())
         temp_range_layout.addWidget(set_temp_range_button)
-        
-        thermal_laser_layout.addLayout(temp_range_layout)
-        
+
         # thermal status label
         self.thermal_label = QtWidgets.QLabel()
         self.thermal_label.setText(wrap_text("Thermal Tracking Disabled", "red"))
@@ -132,6 +126,7 @@ class AutonomyWidget(BaseTabWidget):
 
         # laser controls
         laser_layout = QtWidgets.QHBoxLayout()
+        thermal_laser_layout.addLayout(laser_layout)
 
         laser_on_button = QtWidgets.QPushButton("Laser On")
         laser_on_button.clicked.connect(lambda: self.set_laser(True))
@@ -145,14 +140,7 @@ class AutonomyWidget(BaseTabWidget):
         self.laser_toggle_label.setText(wrap_text("Laser Off", "red"))
         self.laser_toggle_label.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
         laser_layout.addWidget(self.laser_toggle_label)
-
-        thermal_laser_layout.addLayout(laser_layout)
-
-
-        thermal_laser_groupbox.setSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
-
-        custom_layout.addWidget(thermal_laser_groupbox)
-
+        # endregion
 
         # region Magnet control
         """
@@ -162,7 +150,8 @@ class AutonomyWidget(BaseTabWidget):
         magnet_groupbox = QtWidgets.QGroupBox("Magnet Control")
         magnet_layout = QtWidgets.QVBoxLayout()
         magnet_groupbox.setLayout(magnet_layout)
-        
+        layout.addWidget(magnet_groupbox, 1, 1, 1, 1)
+
         magnet_on_btn = QtWidgets.QPushButton("Activate Magnet")
         magnet_on_btn.clicked.connect(lambda: self.set_magnet(True))
         magnet_layout.addWidget(magnet_on_btn)
@@ -173,106 +162,95 @@ class AutonomyWidget(BaseTabWidget):
 
         self.magnet_label = QtWidgets.QLabel()
         self.magnet_label.setText(wrap_text("Magnet Disabled", "red"))
+        self.magnet_label.setSizePolicy(QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Fixed))
         self.magnet_label.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
         magnet_layout.addWidget(self.magnet_label)
-        
-        custom_layout.addWidget(magnet_groupbox)
-        
+        # endregion
+
         # region Testing
         testing_groupbox = QtWidgets.QGroupBox("Test Commands")
         testing_layout = QtWidgets.QVBoxLayout()
         testing_groupbox.setLayout(testing_layout)
+        layout.addWidget(testing_groupbox, 1, 2, 1, 1)
 
-        self.testing_items: list[str] = ['kill', 'arm', 'disarm', 'zero ned'] # List of tests. If you want to add a test just add the name to this list
+        self.testing_items: list[str] = ["kill", "arm", "disarm", "zero ned"]  # List of tests. If you want to add a test just add the name to this list
         self.testing_states: dict[str, QtWidgets.QLabel] = {}
 
         # Create a name label, state label, and on/off buttons for each test
         for item in self.testing_items:
             test_layout = QtWidgets.QHBoxLayout()
+            testing_layout.addLayout(test_layout)
 
             test_name = QtWidgets.QLabel(f"{item.title()} test")
             test_name.setSizePolicy(QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed))
             test_layout.addWidget(test_name)
-            
-            test_state = QtWidgets.QLabel() # Only show the state of the test if it's active
+
+            test_state = QtWidgets.QLabel()  # Only show the state of the test if it's active
             test_state.setSizePolicy(QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed))
+            self.testing_states.update({item: test_state})  # Add the state label to this dict so we can modify it
             test_layout.addWidget(test_state)
-            self.testing_states.update({item: test_state}) # Add the state label to this dict so we can modify it
-            
+
             test_exec_btn = QtWidgets.QPushButton("Execute Test")
-            test_layout.addWidget(test_exec_btn)
             test_exec_btn.clicked.connect(functools.partial(self.set_test, item.lower(), True))
-
-            # Deactivating the test partway through wouldn't do anything, so this button is useless
-            # building_disable_button = QtWidgets.QPushButton("Deactivate Test")
-            # test_layout.addWidget(building_disable_button)
-            # building_disable_button.clicked.connect(functools.partial(self.set_test, item, False))
-
-            testing_layout.addLayout(test_layout)
-
-        custom_layout.addWidget(testing_groupbox)
-
-
-        layout.addLayout(custom_layout, 1, 0, 1, 1) # Finalize second row
+            test_layout.addWidget(test_exec_btn)
+        # endregion
 
         # region Auton positions
-        self.positions: List[str] = [ # List of names for each position
-            "Position 1", "Position 2", "Position 3", "Position 4", "Position 5",
-            "Position 6", "Position 7", "Position 8", "Position 9", "Position 10"
-        ]
-        self.position_states: List[QtWidgets.QLabel] = []
-
         positions_groupbox = QtWidgets.QGroupBox("Positions")
         positions_layout = QtWidgets.QVBoxLayout()
         positions_groupbox.setLayout(positions_layout)
+        positions_groupbox.setSizePolicy(QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding))
+        layout.addWidget(positions_groupbox, 2, 0, 1, 3)
 
-            # ======================
-            # Make each line of position buttons
-        for i in range (len(self.positions)):
+        positions: List[str] = [  # List of names for each position
+            "Land @ Start",
+            "Loiter @ Start",
+            "Position 3",
+            "Position 4",
+            "Position 5",
+            "Position 6",
+            "Position 7",
+            "Position 8",
+            "Position 9",
+            "Position 10",
+        ]
+        self.position_states: List[QtWidgets.QLabel] = []
+
+        # Make each line of position buttons
+        for i in range(len(positions)):
             position_layout = QtWidgets.QHBoxLayout()
+            positions_layout.addLayout(position_layout)
 
-            position_name = QtWidgets.QLabel(self.positions[i])
+            position_name = QtWidgets.QLabel(positions[i])
             position_name.setSizePolicy(QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed))
             position_layout.addWidget(position_name)
-            
+
             position_state = QtWidgets.QLabel("")
             position_state.setSizePolicy(QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed))
             position_layout.addWidget(position_state)
             self.position_states.append(position_state)
-            
+
             position_exec_btn = QtWidgets.QPushButton("Execute Position Command")
             position_layout.addWidget(position_exec_btn)
-            position_exec_btn.clicked.connect(functools.partial(self.set_position, i+1))  # type: ignore
-            
-            positions_layout.addLayout(position_layout)
-        layout.addWidget(positions_groupbox, 2, 0, 4, 1)
-
+            position_exec_btn.clicked.connect(functools.partial(self.set_position, i + 1))
+        # endregion
 
     # region Messengers
-    """
-    NOTE: The reason that label change operations (like when auton is enabled & goes from "Disabled" to "Enabled") are processed in
-    the message handler and not the messaging functions is so we can confirm that the drone gets the command, since the Jetson runs the MQTT server.
-    """
-    # ================
-    # Auton messengers
+    """NOTE: The reason that label change operations (like when auton is enabled & goes from "Disabled" to "Enabled") are processed in
+    the message handler and not the messaging functions is so we can confirm that the drone gets the command, since the Jetson runs the MQTT server."""
+
     def set_position(self, number: int) -> None:
-        # sourcery skip: assign-if-exp
-        """
-        Set the current target position for auton control
-        """
+        """Set the current target position for auton control"""
         self.send_message("avr/autonomous/position", {"position": number})
 
     def set_autonomous(self, state: bool) -> None:
-        """
-        Set autonomous mode
-        """
+        """Set autonomous mode"""
         self.send_message("avr/autonomous/enable", AvrAutonomousEnablePayload(enabled=state))
-    
-    def set_test(self, test_name: str, test_state: bool) -> None:
-        self.send_message('avr/sandbox/test', {'testName': test_name, 'testState': test_state})
 
-    # ==========================
-    # Thermal scanning/targeting messenger
+    def set_test(self, test_name: str, test_state: bool) -> None:
+        """Activate a test"""
+        self.send_message("avr/sandbox/test", {"testName": test_name, "testState": test_state})
+
     def set_thermal_data(self, state: int | None = None) -> None:
         """Handles sending thermal scanning and targeting data
 
@@ -285,11 +263,10 @@ class AutonomyWidget(BaseTabWidget):
         upper = self.temp_max_line_edit.text_float()
         step = self.temp_step_edit.text_float()
 
-        self.send_message('avr/autonomous/thermal_data', {'state': self.thermal_state, 'range': (lower, upper, step)})
+        self.send_message("avr/autonomous/thermal_data", {"state": self.thermal_state, "range": (lower, upper, step)})
 
-    # ==============================
-    # Laser messenger
     def set_laser(self, state: bool) -> None:
+        """Enable/disable laser firing"""
         if state:
             topic = "avr/pcm/set_laser_on"
             payload = AvrPcmSetLaserOnPayload()
@@ -298,28 +275,27 @@ class AutonomyWidget(BaseTabWidget):
             payload = AvrPcmSetLaserOffPayload()
 
         self.send_message(topic, payload)
-    
-    # =======================
-    # Magnet messenger
+
     def set_magnet(self, enabled: bool) -> None:
         """Enable/disable magnet power
 
         MAGNETS AND LASERS ARE MUTUALLY EXCLUSIVE
         """
         self.send_message("avr/pcm/set_magnet", {"enabled": enabled})
-    
+
+    # endregion
 
     # region MQTT Handler
     def process_message(self, topic: str, payload: dict) -> None:
         # sourcery skip: low-code-quality
         # Yeah, you're telling me
-        """
-        Processes incoming messages based on the specified topic and updates the UI accordingly.
+        """Processes incoming messages based on the specified topic and updates the UI accordingly.
         This function handles various topics related to autonomous operations, including enabling/disabling autonomy, updating positions, managing test states, and handling thermal and object scanner data.
         """
         payload = json.loads(payload)
-        if topic == "avr/autonomous/enable": # If the value of the auton bool is changing
-            state = payload['enabled']
+
+        if topic == "avr/autonomous/enable":  # If the value of the auton bool is changing
+            state = payload["enabled"]
             if state:
                 text = "Autonomous Enabled"
                 color = "green"
@@ -328,22 +304,22 @@ class AutonomyWidget(BaseTabWidget):
                 color = "red"
             self.autonomous_label.setText(wrap_text(text, color))
         elif topic == "avr/autonomous/position":
-            pos_num = payload['position']
+            pos_num = payload["position"]
             if pos_num == 0:
                 for state in self.position_states:
                     state.setText("")
                 return
             else:
-                self.position_states[pos_num-1].setText(wrap_text("Executing position command...", "red"))
-        elif topic == "avr/sandbox/test": # If we're activating or deactivating a test
-            name = payload['testName']
-            state = payload['testState']
+                self.position_states[pos_num - 1].setText(wrap_text("Executing position command...", "red"))
+        elif topic == "avr/sandbox/test":  # If we're activating or deactivating a test
+            name = payload["testName"]
+            state = payload["testState"]
             if state:
                 self.testing_states[name].setText(wrap_text("Executing...", "red"))
             else:
                 self.testing_states[name].setText("")
         elif topic == "avr/autonomous/thermal_data":
-            match payload['state']:
+            match payload["state"]:
                 case 2:
                     text = "Thermal Tracking Enabled"
                     color = "green"
@@ -373,19 +349,20 @@ class AutonomyWidget(BaseTabWidget):
             for key in payload.keys():
                 self.topic_status_map[key].set_health(payload[key])
         elif topic == "avr/autonomous/sound":
-            file_name = payload['fileName']
-            ext = payload['ext']
-            if 'max_vol' in payload.keys():
-                max_volume: bool = payload['max_vol']
+            file_name = payload["fileName"]
+            ext = payload["ext"]
+            if "max_vol" in payload.keys():
+                max_volume: bool = payload["max_vol"]
 
             playsound.playsound(f"./GUI/assets/sounds/{file_name}{ext}", False)
 
             if max_volume:
                 # Get the default audio device
                 devices = AudioUtilities.GetSpeakers()
-                interface = devices.Activate(
-                    IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+                interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
                 volume = cast(interface, POINTER(IAudioEndpointVolume))
 
-                while True: # Set the volume to maximum (1.0 represents 100%)
+                while True:  # Set the volume to maximum (1.0 represents 100%)
                     volume.SetMasterVolumeLevelScalar(1.0, None)
+
+    # endregion
