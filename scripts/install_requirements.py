86492774b2eb35594da8ef464a401694104efd8e
Max Haberer <mhaberer25@gmail.com>
2024-09-21 00:07:49 -0500

    Update install requirements script and task, and bump numpy to most recent
    - New messages in install_requirements.py will make the process of the script easier to understand in the future
    - Install requirements task will now reveal the terminal so user's can track the task's completion
    - Updated numpy version: outdated version was causing issues when installing requirements

--- scripts/install_requirements.py
+++ scripts/install_requirements.py
@@ -4,9 +4,31 @@ import subprocess
 import sys
 from pathlib import Path
 
+CYAN = "\033[0;36m"
+NO_COLOR = "\033[0m"
+
+
+def disp_added_sidebars(text: str | None = None, color_code: str = NO_COLOR) -> None:
+    """Adds "sidebars" in the form of `=` to each side of a string and prints it
+
+    Args:
+        text (str | None, optional): The string blessed with "sidebars". Leaving this blank will print a full line of `=`
+        color_code (str, optional): Escape code of a certain color to wrap the text in. Leaving this blank will not color the text. I recommend sourcing color codes from `setup.py`
+    """
+    if text is None:
+        msg = "=" * os.get_terminal_size().columns
+    else:
+        void_len = len(text) + 2  # Define the "void" area where the string will go
+        sidebar_len = (os.get_terminal_size().columns - void_len) // 2  # Define the length of a single sidebar
+        sidebar = "=" * sidebar_len  # Make the sidebars
+        msg = f"{sidebar} {color_code}{text}{NO_COLOR} {sidebar}"
+    print(msg)
+
 
 def main(directory: str, strict: bool = False) -> None:
+    AVR_DIR = Path(os.path.join(os.path.dirname(directory), ".")).resolve()
     # make sure pip and wheel are up-to-date
+    disp_added_sidebars("Pre-execution checks", CYAN)
     subprocess.run(
         [sys.executable, "-m", "pip", "install", "wheel", "pip", "--upgrade"],
         check=True,
@@ -23,6 +45,7 @@ def main(directory: str, strict: bool = False) -> None:
         sys.exit(1)
 
     # install dev tools
+    disp_added_sidebars("Installing dev tools", CYAN)
     subprocess.run(
         [
             sys.executable,
@@ -42,7 +65,7 @@ def main(directory: str, strict: bool = False) -> None:
         if ".venv" in str(filepath):
             continue
 
-        print(f" ----- Installing {filepath.absolute()} ----- ")
+        disp_added_sidebars(f"{filepath.relative_to(AVR_DIR)}", CYAN)
         subprocess.run(
             [
                 sys.executable,
