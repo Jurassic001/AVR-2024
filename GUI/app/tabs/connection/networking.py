61a76c098e04ab05b9aacaaded6e6f30c9b96905
Max Haberer <mhaberer25@gmail.com>
2024-10-04 21:41:20 -0500

    Refactor WIFIConnectionWidget network connection and checking
    - Updated the network_connect method to include a call to the new network_check method, which checks the connection to the specified network for a set number of times.
    - Added the network_check method, which checks the connection to the network for a specified number of times and updates the connection status label accordingly.
    - Created the single_network_check method, which checks the connection to the network once and updates the connection status label.

--- GUI/app/tabs/connection/networking.py
+++ GUI/app/tabs/connection/networking.py
@@ -31,21 +31,41 @@ class WIFIConnectionWidget(QtWidgets.QWidget):
 
         layout.addLayout(network_layout)
 
-        self.network_connect()
+        self.single_network_check()
 
     def network_connect(self) -> None:
-        # attempt to connect to the specified network
+        """Attempt to connect to the specified network"""
         network_name = self.network_name_lnedit.text()
         subprocess.run(["netsh", "wlan", "connect", f"name={network_name}"], capture_output=True)
+        self.network_check(5)
 
-        for _ in range(10):
-            # check if we connected successfully ten times, once every 100ms
-            time.sleep(0.1)
+    def network_check(self, check_attempts: int) -> None:
+        """Check the connection to the network (which is named in the line edit) for a set number of times
+
+        Args:
+            check_attempts (int): The number of times we check to see if we are connected. Each check takes roughly 150 to 300 milliseconds
+        """
+        network_name = self.network_name_lnedit.text()
+        start_time = time.time()
+
+        for _ in range(check_attempts):
+            # check if we connected successfully
             check_network = subprocess.run(["netsh", "wlan", "show", "interfaces"], capture_output=True, text=True)
             if network_name in check_network.stdout:
                 # update the status label and network config on successful connection
-                self.connection_label.setText(wrap_text(f'Successfully connected to "{network_name}"', "Green"))
+                self.connection_label.setText(wrap_text(f'Successfully connected to "{network_name}" after {round(time.time() - start_time, 2)} seconds', "Green"))
                 config.network_name = network_name
                 return
-        # if the connection cannot be established in one second, then assume the connection attempt has failed
-        self.connection_label.setText(wrap_text(f'Failed to connect to "{network_name}"', "Red"))
+            # if we haven't connected, wait for 50ms before trying again
+            time.sleep(0.05)
+        # if the connection cannot be established, then assume the connection attempt has failed
+        self.connection_label.setText(wrap_text(f'Failed to connect to "{network_name}" after {round(time.time() - start_time, 2)} seconds', "Red"))
+
+    def single_network_check(self) -> None:
+        """Check the connection to the network (which is named in the line edit) once. Does not set the value of the network_name in the config. Only intended to be used for initial connection check"""
+        network_name = self.network_name_lnedit.text()
+        check_network = subprocess.run(["netsh", "wlan", "show", "interfaces"], capture_output=True, text=True)
+        if network_name in check_network.stdout:
+            self.connection_label.setText(wrap_text(f'Connected to "{network_name}"', "Green"))
+        else:
+            self.connection_label.setText(wrap_text(f'Not connected to "{network_name}"', "Red"))
